f37a5793f430cd22becda16c3a37c89f8fec35c2
Nesgomez9 <nicolico99@hotmail.com>
2020-02-05 18:27:06 +0000

    adding first mandatory task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts a array of numbers with a bubble sort algorithm
+ * @array: array of ints
+ * @size: len of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] < array[j + 1])
+				tmp = array[j + 1];
+			else
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+
+			}
+		}
+	}
+}
---
+++ sort.h
@@ -0,0 +1,35 @@
+#ifndef STRUCT
+#define STRUCT
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+#endif /* STRUCT */
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <stdlib.h>
+#include <stdio.h>
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void swaper(int *array, size_t size, int *a, int *b);
+void list_swaper(listint_t **list, listint_t *current, listint_t *next);
+void quick_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void shell_sort(int *array, size_t size);
+size_t maxgap(size_t size);
+#endif
