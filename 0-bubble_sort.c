44c84304c9a6a077f6f5ae744b7d2417b6b028ed
Nesgomez9 <nicolico99@hotmail.com>
2020-02-05 18:29:30 +0000

    adding first mandatory task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,13 +9,14 @@ void bubble_sort(int *array, size_t size)
 	int tmp;
 	size_t i, j;
 
+	if (!size || !array)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j] < array[j + 1])
-				tmp = array[j + 1];
-			else
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
